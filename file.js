8af6ce6cbf10535cd8dfd2af996cd6155aa79de0
DeekshithSH <shdeekshith@gmail.com>
2023-07-26 19:39:43 +0530

    some bad css

--- file.js
+++ file.js
@@ -12,6 +12,14 @@ async function main() {
     const response = await fetch(`${baseurl}/file?device=${device}&type=${type}&id=${id}`);
     const data = await response.json();
 
+    var script = document.createElement("script");
+    script.src = "https://telegram.org/js/telegram-widget.js?5";
+    script.setAttribute("data-telegram-post", `${data["channel_username"]}/${data["msg_id"]}`);
+    script.setAttribute("data-userpic", "true")
+    script.setAttribute("data-width", "100%");
+    script.setAttribute("data-dark", "1")
+    document.getElementById("tg-post").appendChild(script);
+
     update_text("build-name", data["name"] + " " + data["version"])
     update_text("type", type)
     update_text("dev", "by " + data["dev"])
